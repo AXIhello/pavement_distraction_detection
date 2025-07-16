fe642fa09a07cf7d9115e5ff9bc19d6b889b20dd
AXI <3306778393@qq.com>
2025-07-16 17:30:34 +0800

    前后端接入：时空交通分析

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,10 +16,21 @@ export default defineConfig({
     },
   },
   server: {
+    // 本地运行
+    host: '127.0.0.1',
+    port: 5173,
     proxy: {
+      // 交通分析API转发到Django
       '/api/traffic_analysis': {
-        target: 'http://127.0.0.1:9000',
+        target: 'http://10.61.169.63:9000',
         changeOrigin: true,
+        secure: false
+      },
+      // 其他API转发到Flask
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false
       }
     }
   }
